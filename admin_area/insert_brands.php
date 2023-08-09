583a11f03e22d4a39e22231cd595acb786449b8b
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-08-09 12:49:22 +0600

    insert categories

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,7 +1,30 @@
+<?php
+include('../includes/connect.php');
+
+if(isset($_POST['insert_brand'])){
+    $brand_title=$_POST['brand_title'];
+    //select data from database to stop repetation
+$select_query= "SELECT * FROM `brands` where brand_title = '$brand_title' ";
+$result_select = mysqli_query($con,$select_query);
+$number = mysqli_num_rows($result_select);
+if($number > 0){
+    echo "<script>alert('This Brand is already present in the database')</script>";
+}else{
+    $query= "INSERT INTO `brands` (`brand_title`) VALUES ('$brand_title');    ";
+$result = mysqli_query($con,$query);
+if($result){
+    echo "<script>alert('Brand has been inserted successfully')</script>";
+}}
+}
+
+
+?>
+
+
 <form action="" method="post">
 <div class="input-group mb-3">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i> </span>
-  <input type="text" class="form-control" placeholder="Insert Brands" aria-label="Insert_brands" aria-describedby="basic-addon1">
+  <input type="text" name="brand_title" class="form-control" placeholder="Insert Brands" aria-label="Insert_brands" aria-describedby="basic-addon1">
 </div>
-<button type="submit" class="btn btn-info rounded-0">Insert Brands</button>
+<input type="submit" name="insert_brand" value="Insert Brand" class="btn btn-info rounded-0"></input>
 </form>
--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,7 +1,29 @@
+<?php
+include('../includes/connect.php');
+
+if(isset($_POST['insert_cat'])){
+    $category_title=$_POST['category_title'];
+    //select data from database to stop repetation
+$select_query= "SELECT * FROM `categories` where category_title = '$category_title' ";
+$result_select = mysqli_query($con,$select_query);
+$number = mysqli_num_rows($result_select);
+if($number > 0){
+    echo "<script>alert('This category is already present in the database')</script>";
+}else{
+    $query= "INSERT INTO `categories` (`category_title`) VALUES ('$category_title');    ";
+$result = mysqli_query($con,$query);
+if($result){
+    echo "<script>alert('Category has been inserted successfully')</script>";
+}}
+}
+
+
+?>
+
 <form action="" method="post">
 <div class="input-group mb-3">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i> </span>
-  <input type="text" class="form-control" placeholder="Insert Categories" aria-label="Insert_categories" aria-describedby="basic-addon1">
+  <input type="text" name="category_title" class="form-control" placeholder="Insert Categories" aria-label="Insert_categories" aria-describedby="basic-addon1">
 </div>
-<button type="submit" class="btn btn-info rounded-0">Insert Categories</button>
+<input type="submit" name="insert_cat" value="Insert Categories" class="btn btn-info rounded-0"></input>
 </form>
