65393f8e872ca194263ada896a5b5900a89aa689
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-08-09 11:12:02 +0600

    frontend

---
+++ admin_area/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+     <!-- bootstrap link -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!-- fontawosome link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- css link -->
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+<div class="container-fluid p-0">
+    <!-- first child  -->
+    <nav class="navbar navbar-expand-lg bg-info navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Logo</a>
+  
+    <div class="navbar navbar-expand-lg bg-info navbar-light">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Welcome Guest</a>
+        </li>
+</ul>
+</div>
+</div>
+</nav>
+<!-- second child  -->
+<div class="container-fluid bg-light text-center p-2">
+    <h1>Manage Details</h1>
+</div>
+
+<!-- third child  -->
+<div class="row">
+<div class="col-md-12 d-flex bg-secondary align-items-center">
+    <div class="px-3">
+        <img src="" alt="" class="adminImage">
+        <p class="text-center text-light">Admin name</p>
+    </div>
+    <div class="button text-center">
+    <button><a href="" class="nav-link bg-info text-light p-2">Insert Products</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">View Products</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">Insert Categories</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">View Categories</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">Insert Brands</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">View Brands</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">All Orders </a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">All Payments</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">Users List</a></button>
+    <button><a href="" class="nav-link bg-info text-light p-2">Logout</a></button>
+</div>
+</div>
+</div>
+
+
+
+    <!-- last-child  -->
+    <div class="container-fluid p-3 text-center bg-info footer">
+      <p>All rights reserved @- juu-2023</p>
+    </div>
+    </div>
+<!-- bootsrap js link  -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-COmmerse using PHP</title>
+    <!-- bootstrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!-- fontawosome link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg bg-info navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping fa-bounce" style="color: #050505;"></i> <sup>1</sup></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total Price: 500/-</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2 rounded-1" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
+<!-- second child  -->
+<div class="navbar navbar-expand-lg bg-secondary">
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>   <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+</ul>
+</div>
+<!-- third child  -->
+<div class="bg-light text-center">
+  <h3>Hidden Store</h3>
+  <p>Communication is the heart o ecommere and community</p>
+</div>
+<!-- fourth-child  -->
+<div class="row">
+  <div class="col-md-10">
+    <!-- Products  -->
+    <div class="row">
+      <div class="col-md-4 mb-2">
+      <div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-2 bg-secondary p-0">
+    <!-- brands  -->
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center text-light ">
+      <li class="nav-item mb-2 bg-info"><h3>Delivery Brands</h3></li>
+      <li class="nav-item mb-2"><a href=""></a>Brand 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Brand 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Brand 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Brand 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Brand 1</li>
+    </ul>
+    
+    <!-- categories  -->
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center text-light ">
+      <li class="nav-item mb-2 bg-info"><h3>Categories</h3></li>
+      <li class="nav-item mb-2"><a href=""></a>Category 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Category 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Category 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Category 1</li>
+      <li class="nav-item mb-2"><a href=""></a>Category 1</li>
+    </ul>
+
+  </div>
+</div>
+<!-- last-child  -->
+    <div class="container-fluid p-3 text-center bg-info">
+      <p>All rights reserved @- juu-2023</p>
+    </div>
+<!-- bootsrap js link  -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
