b41f83ad4c9490751d1a938cf6b6035e93821421
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-08-16 13:36:57 +0600

    displaying cart items

--- cart.php
+++ cart.php
@@ -15,7 +15,7 @@ include('./functions/common_functions.php');
   <!-- fontawosome link  -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- css link -->
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
@@ -82,27 +82,51 @@ include('./functions/common_functions.php');
             <th>Quantity</th>
             <th>Total Price</th>
             <th>Remove</th>
-            <th>Operations</th>
+            <th class="col-span-2">Operations</th>
         </tr>
     </thead>
     <tbody>
+        <?php
+          global $con;
+          $total_price = 0;
+          $get_ip_address = getIPAddress();
+          $cart_query = "SELECT * FROM `cart_details` WHERE ip_address='$get_ip_address'";
+          $result_cart = mysqli_query($con, $cart_query);
+          while ($row_data = mysqli_fetch_array($result_cart)) {
+            $product_id = $row_data['product_id'];
+            $product_query = "SELECT * FROM `products` WHERE product_id=$product_id";
+            $result_products = mysqli_query($con, $product_query);
+            while ($row_product_price = mysqli_fetch_array($result_products)) {
+              $product_price_array = array($row_product_price['product_price']);
+              $product_price_sum = array_sum($product_price_array);
+              $product_title= $row_product_price['product_title'];
+              $product_price= $row_product_price['product_price'];
+              $product_image1= $row_product_price['product_image1'];
+              // echo "$product_price_sum";
+         
+        ?>
         <tr>
-            <td>Apple</td>
-            <td><img src="" alt=""></td>
-            <td><input type="text"></td>
-            <td>5</td>
+            <td><?php echo $product_title?></td>
+            <td><img class="cart-image" src="./admin_area/product_images/<?php echo $product_image1?>" alt=""></td>
+            <td><input type="form input w-50"></td>
+            <td><?php echo $product_price?></td>
             <td><input type="checkbox"></td>
             <td>
-                <p>Update</p>
-                <p>Remove</p>
+                <p class="btn btn-info">Update</p>
+                <p class="btn btn-info">Remove</p>
             </td>
         </tr>
+        <?php
+           }
+           $total_price += $product_price_sum;
+         }
+         ?>
     </tbody>
   </table>
 
   <!-- subtotal  -->
 <div class="d-flex gap-3 my-3">
-    <h4>Subtotal: <strong class="text-info">5000/-</strong></h4>
+    <h4>Subtotal: <strong class="text-info"><?php echo $total_price?>/-</strong></h4>
     <a href="index.php"><button class="btn btn-info rounded-0">Continue Shopping</button></a>
     <a href=""><button class="btn btn-secondary rounded-0">Checkout</button></a>
 </div>
--- shared/footer.php
+++ shared/footer.php
@@ -1,3 +1,3 @@
-<div class="container-fluid p-3 text-center bg-info footer">
+<div class="container-fluid p-3 text-center bg-info ">
     <p>All rights reserved @- juu-2023</p>
   </div>
