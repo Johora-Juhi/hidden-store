7bcf2626db2ffb3944cbc0aeec42918db5a449da
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-08-28 13:12:30 +0600

    user registration

--- users_area/checkout.php
+++ users_area/checkout.php
@@ -29,10 +29,10 @@ include('../functions/common_functions.php');
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="display_all.php">Products</a>
+              <a class="nav-link" href="../display_all.php">Products</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Register</a>
