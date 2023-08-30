0f1e28c5bb98a9cc4a60070ab0a29a1f99cbd76d
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-08-30 13:41:58 +0600

    order placed

--- users_area/order.php
+++ users_area/order.php
@@ -11,7 +11,7 @@ $total_quantity =0;
   $get_ip_address = getIPAddress();
   $cart_query = "SELECT * FROM `cart_details` WHERE ip_address='$get_ip_address'";
   $result_cart = mysqli_query($con, $cart_query);
-//   $total_product = mysqli_num_rows($result_cart); 
+  $total_product = mysqli_num_rows($result_cart); 
   while ($row_data = mysqli_fetch_array($result_cart)) {
     $product_id = $row_data['product_id'];
     $product_quantity = $row_data['quantity'];
@@ -36,10 +36,18 @@ $total_quantity =0;
   $order_status = 'pending';
 
 // insert_order 
-$insert_order = "INSERT INTO `user_orders` (user_id,amount_due,invoice_number,total_products,order_date,order_status) VALUES ($user_id,$total_price,$invoice_number,$total_quantity,NOW(),'$order_status')";
+$insert_order = "INSERT INTO `user_orders` (user_id,amount_due,invoice_number,total_products,order_date,order_status) VALUES ($user_id,$total_price,$invoice_number,$total_product,NOW(),'$order_status')";
 $result_order = mysqli_query($con,$insert_order);
 if($result_order){
     echo "<script>alert('Order placed sucessfully')</script>";
     echo "<script>window.open('./profile.php','_self')</script>";
 }
+
+// pending_orders 
+$pending_order = "INSERT INTO `orders_pending` (user_id,invoice_number,product_id,quantity,orders_status) VALUES ($user_id,$invoice_number,$product_id,$total_quantity,'$order_status')";
+$result_pending_order = mysqli_query($con,$pending_order);
+
+// delete from cart table 
+$delete_cart = "DELETE FROM `cart_details` where ip_address='$get_ip_address'";
+$result_delete= mysqli_query($con,$delete_cart);
 ?>
