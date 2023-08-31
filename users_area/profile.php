18664795159dcd5a350edb4e0a12c01d28e2edf1
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-08-31 12:10:26 +0600

    profile image

--- users_area/profile.php
+++ users_area/profile.php
@@ -10,13 +10,13 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "Welcome " . $_SESSION['user_email'];?></title>
+    <title><?php echo "Welcome " . $_SESSION['user_email']; ?></title>
     <!-- bootstrap link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!-- fontawosome link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- css link -->
-<link rel="stylesheet" href="../style.css">    
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
@@ -44,7 +44,6 @@ session_start();
                         <li class="nav-item">
                             <a class="nav-link" href="#">Total Price: <?php totalCartPrice(); ?>/-</a>
                         </li>
-
                     </ul>
                     <form class="d-flex" action="../search_product.php" method="get" role="search">
                         <input class="form-control me-2 rounded-1" type="search" name="search_data" placeholder="Search" aria-label="Search">
@@ -103,27 +102,32 @@ session_start();
             <ul class="navbar-nav bg-secondary text-center text-light">
                 <li class='nav-item bg-info'>
                     <a class='nav-link' href=''>
-                        <h4>Your Profile</h4></a>
+                        <h4>Your Profile</h4>
+                    </a>
+                </li>
+                <?php
+                $username=$_SESSION['user_email'];
 
-                </li>
+                $select_user= "SELECT * FROM `user_table` where username = '$username'";
+                $result_user = mysqli_query($con,$select_user);
+                $user = mysqli_fetch_assoc($result_user);
+                $user_image = $user['user_image'];
+                echo "
                 <li class='nav-item'>
-                    <img class="profile_img" src="./user_images/artist2.jpg" alt="">
-                </li>
+                    <img class='profile_img' src='./user_images/$user_image' alt=''>
+                </li>"
+                ?>
                 <li class='nav-item'>
                     <a class='nav-link' href=''>Pending Orders</a>
-
                 </li>
                 <li class='nav-item'>
                     <a class='nav-link' href=''>Edit Account</a>
-
                 </li>
                 <li class='nav-item'>
                     <a class='nav-link' href=''>My orders</a>
-
                 </li>
                 <li class='nav-item'>
                     <a class='nav-link' href=''>Delete Account</a>
-
                 </li>
                 <li class='nav-item'>
                     <a class='nav-link' href='./logout.php'>Logout</a>
