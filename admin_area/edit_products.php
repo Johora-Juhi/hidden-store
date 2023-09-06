e34e80da26ae43f4e821a0d7144ca57a9b13c3ad
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-09-06 14:51:25 +0600

    edit products complete

--- admin_area/edit_products.php
+++ admin_area/edit_products.php
@@ -10,15 +10,15 @@ if (isset($_GET['edit_products'])) {
     $category_id = $products['category_id'];
     // select choosen category 
     $select_category = "SELECT * FROM `categories` where category_id = $category_id";
-    $result_category = mysqli_query($con,$select_category);
+    $result_category = mysqli_query($con, $select_category);
     $category = mysqli_fetch_assoc($result_category);
     $category_title = $category['category_title'];
     $barnd_id = $products['barnd_id'];
-       // select choosen category 
-       $select_brand = "SELECT * FROM `brands` where brand_id = $barnd_id";
-       $result_brand = mysqli_query($con,$select_brand);
-       $brand = mysqli_fetch_assoc($result_brand);
-       $brand_title = $brand['brand_title'];
+    // select choosen category 
+    $select_brand = "SELECT * FROM `brands` where brand_id = $barnd_id";
+    $result_brand = mysqli_query($con, $select_brand);
+    $brand = mysqli_fetch_assoc($result_brand);
+    $brand_title = $brand['brand_title'];
     $product_title = $products['product_title'];
     $product_image1 = $products['product_image1'];
     $product_image2 = $products['product_image2'];
@@ -34,22 +34,22 @@ if (isset($_GET['edit_products'])) {
     <!-- title  -->
     <div class="form-outline mb-4 w-50 mx-auto">
         <label for="product_title" class="form-label">Product Title</label>
-        <input type="text" id="product_title" name="product_title" class="form-control" value="<?php echo $product_title ?>" placeholder="Enter product title" autocomplete="off" required>
+        <input type="text" id="product_title" name="product_title" class="form-control" value="<?php echo $product_title ?>" placeholder="Enter product title" autocomplete="off">
     </div>
     <!-- description  -->
     <div class="form-outline mb-4 w-50 mx-auto">
         <label for="product_description" class="form-label">Product Description</label>
-        <input type="text" id="product_description" name="product_description" class="form-control" value="<?php echo $product_description ?>" placeholder="Enter product description" autocomplete="off" required>
+        <input type="text" id="product_description" name="product_description" class="form-control" value="<?php echo $product_description ?>" placeholder="Enter product description" autocomplete="off">
     </div>
     <!-- keywords  -->
     <div class="form-outline mb-4 w-50 mx-auto">
         <label for="product_keywords" class="form-label">Product Keywords</label>
-        <input type="text" id="product_keywords" name="product_keywords" class="form-control" value="<?php echo $product_keywords ?>" placeholder="Enter product keywords" autocomplete="off" required>
+        <input type="text" id="product_keywords" name="product_keywords" class="form-control" value="<?php echo $product_keywords ?>" placeholder="Enter product keywords" autocomplete="off">
     </div>
     <!-- category  -->
     <div class="form-outline mb-4 w-50 mx-auto">
         <select name="product_category" id="" class="form-select">
-            <option value="<?php echo $category_title?>"><?php echo $category_title?></option>
+            <option value="<?php echo $category_id ?>" selected><?php echo $category_title ?></option>
             <?php
             $selct_categories = 'SELECT * FROM `categories`';
             $result_categories = mysqli_query($con, $selct_categories);
@@ -64,7 +64,7 @@ if (isset($_GET['edit_products'])) {
     <!-- brand  -->
     <div class="form-outline mb-4 w-50 mx-auto">
         <select name="product_brand" id="" class="form-select">
-            <option value="<?php echo $brand_title?>"><?php echo $brand_title?></option>
+            <option value="<?php echo $barnd_id ?>" selected><?php echo $brand_title ?></option>
             <?php
             $selct_brands = 'SELECT * FROM `brands`';
             $result_brands = mysqli_query($con, $selct_brands);
@@ -80,7 +80,7 @@ if (isset($_GET['edit_products'])) {
     <div class="form-outline mb-4 w-50 mx-auto">
         <label for="product_image1" class="form-label">Product Image 1</label>
         <div class="d-flex">
-            <input type="file" id="product_image1" name="product_image1" class="form-control" required>
+            <input type="file" id="product_image1" name="product_image1" class="form-control">
             <img src="./product_images/<?php echo $product_image1 ?>" class="product_img" alt="">
         </div>
     </div>
@@ -88,7 +88,7 @@ if (isset($_GET['edit_products'])) {
     <div class="form-outline mb-4 w-50 mx-auto">
         <label for="product_image2" class="form-label">Product Image 2</label>
         <div class="d-flex">
-            <input type="file" id="product_image2" name="product_image2" class="form-control" required>
+            <input type="file" id="product_image2" name="product_image2" class="form-control">
             <img src="./product_images/<?php echo $product_image2 ?>" class="product_img" alt="">
         </div>
     </div>
@@ -96,17 +96,60 @@ if (isset($_GET['edit_products'])) {
     <div class="form-outline mb-4 w-50 mx-auto">
         <label for="product_image3" class="form-label">Product Image 3</label>
         <div class="d-flex">
-            <input type="file" id="product_image3" name="product_image3" class="form-control" required>
+            <input type="file" id="product_image3" name="product_image3" class="form-control">
             <img src="./product_images/<?php echo $product_image3 ?>" class="product_img" alt="">
         </div>
     </div>
     <!-- price  -->
     <div class="form-outline mb-4 w-50 mx-auto">
         <label for="product_price" class="form-label">Product Price</label>
-        <input type="text" id="product_price" name="product_price" class="form-control" value="<?php echo $product_price?>" placeholder="Enter product price" autocomplete="off" required>
+        <input type="text" id="product_price" name="product_price" class="form-control" value="<?php echo $product_price ?>" placeholder="Enter product price" autocomplete="off">
     </div>
     <!-- Submit  -->
     <div class="form-outline mb-4 w-50 mx-auto">
-        <input type="submit" name="edit_product" class="btn btn-info rounded-0 px-3 mb-3" value="Update" required>
+        <input type="submit" name="edit_product" class="btn btn-info rounded-0 px-3 mb-3" value="Update">
     </div>
 </form>
+
+<?php
+if (isset($_POST['edit_product'])) {
+    $product_title = $_POST['product_title'];
+    $product_description = $_POST['product_description'];
+    $product_keywords = $_POST['product_keywords'];
+    $product_category = $_POST['product_category'];
+    $product_brand = $_POST['product_brand'];
+    $product_price = $_POST['product_price'];
+    $new_product_image1 = $_FILES['product_image1']['name'];
+    $new_product_image2 = $_FILES['product_image2']['name'];
+    $new_product_image3 = $_FILES['product_image3']['name'];
+    if ($new_product_image1) {
+        $update_image1 = $new_product_image1;
+        $temp_product_image1 = $_FILES['product_image1']['tmp_name'];
+        move_uploaded_file($temp_product_image1, "./product_images/$update_image1");
+    } else {
+        $update_image1 = $product_image1;
+    }
+    if ($new_product_image2) {
+        $update_image2 = $new_product_image2;
+        $temp_product_image2 = $_FILES['product_image2']['tmp_name'];
+        move_uploaded_file($temp_product_image2, "./product_images/$update_image2");
+    } else {
+        $update_image2 = $product_image2;
+    }
+    if ($new_product_image3) {
+        $update_image3 = $new_product_image3;
+        $temp_product_image3 = $_FILES['product_image3']['tmp_name'];
+        move_uploaded_file($temp_product_image3, "./product_images/$update_image3");
+    } else {
+        $update_image3 = $product_image3;
+    }
+
+    $update_product = "Update `products` SET product_title = '$product_title', product_description = '$product_description', product_keywords = '$product_keywords', category_id = $product_category, barnd_id=$product_brand, product_image1 = '$update_image1', product_image2 = '$update_image2', product_image3 = '$update_image2', product_price = '$product_price', date= NOW() where product_id = $edit_id";
+
+    $result_products= mysqli_query($con,$update_product);
+    if($result_products){
+        echo "<script>alert('Product Updated')</script>";
+        echo "<script>window.open('./index.php?view_products','_self')</script>";
+    }
+}
+?>
